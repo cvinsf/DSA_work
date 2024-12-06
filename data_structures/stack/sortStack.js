382d29e45081b947f4c0c7e80237195fbbb761aa
Carlos Villarreal <lquilpensl@gmail.com>
2024-12-05 16:34:26 -0800

    added sortStack.js

---
+++ data_structures/stack/sortStack.js
@@ -0,0 +1,7 @@
+/**
+ ** Given a stack, sort it using only stack operations (push and pop).
+ ** You can use an additional temporary stack, but you may not copy the elements 
+ ** into any other data structure (such as an array). 
+ ** The values in the stack are to be sorted in descending order, 
+ ** with the largest elements on top.
+ */
